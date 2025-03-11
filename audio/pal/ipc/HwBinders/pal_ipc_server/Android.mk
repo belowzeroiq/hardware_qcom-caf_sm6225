abfc8df0b64cf5ba5979f7c46a5cb9f751f3186a
De <162432464+belowzeroiq@users.noreply.github.com>
2025-03-11 22:04:05 +0700

    Initial sm6225 hals

---
+++ audio/pal/ipc/HwBinders/pal_ipc_server/Android.mk
@@ -0,0 +1,39 @@
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(QCPATH),)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := vendor.qti.hardware.pal@1.0-impl
+LOCAL_MODULE_OWNER := qti
+LOCAL_VENDOR_MODULE := true
+LOCAL_CFLAGS += -v
+LOCAL_CFLAGS += -Wno-unused-parameter
+LOCAL_CFLAGS += -Wno-unused-variable
+LOCAL_CFLAGS += -Wno-format
+LOCAL_CFLAGS += -Wno-missing-field-initializers
+LOCAL_CFLAGS += -Wno-unused-function
+LOCAL_SRC_FILES := \
+    src/pal_server_wrapper.cpp
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc
+
+ifeq ($(QCPATH),)
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/../../..
+endif
+
+LOCAL_SHARED_LIBRARIES := \
+    libhidlbase \
+    libhidltransport \
+    libutils \
+    liblog \
+    libcutils \
+    libhardware \
+    libbase \
+    vendor.qti.hardware.pal@1.0 \
+    libar-pal
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
